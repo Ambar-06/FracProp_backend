24405e40fd437db4e720f87b1eb9695f0401fe53
Ambar Rizvi <brannstrom9911@gmail.com>
2024-12-28 16:54:09 +0530

    initial setup

---
+++ user/models/user.py
@@ -0,0 +1,24 @@
+from django.db import models
+from common.models.base_model import BaseModel
+
+class User(BaseModel):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=14, null=True, blank=True)
+    country_code = models.CharField(max_length=5, null=True, blank=True)
+    is_email_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+    password = models.CharField(max_length=255, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_guest = models.BooleanField(default=False)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.uuid)
+    
+    class Meta:
+        unique_together = ('country_code', 'phone_number')
