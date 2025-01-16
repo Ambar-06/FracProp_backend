01775bd4cf903bd3fbdd3bc74c1f3dc6d9c7c345
Ambar Rizvi <brannstrom9911@gmail.com>
2025-01-16 15:53:28 +0530

    changed return models

--- investment/models/investment_return.py
+++ investment/models/investment_return.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 from common.models.base_model import BaseModel
-from investment.models.investment import Investment
 from user.models.user import User
 
 
@@ -9,9 +8,8 @@ class InvestmentReturn(BaseModel):
     user = models.ForeignKey(
         User, on_delete=models.DO_NOTHING, null=True, blank=True, related_name="returns"
     )
-    investment = models.ForeignKey(
-        Investment, on_delete=models.DO_NOTHING, null=True, blank=True
-    )
     return_in_amount = models.FloatField(null=True)
     return_in_percent = models.FloatField(null=True)
+    amount_settled = models.FloatField(null=True)
     credited_to_account = models.BooleanField(default=False)
+    amount_credit_date = models.DateTimeField()
