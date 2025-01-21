96bafa6f8b8d850b688580be95268658a7dce0b3
Ambar Rizvi <brannstrom9911@gmail.com>
2025-01-21 17:39:17 +0530

    fixed urls

--- user/urls.py
+++ user/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("auth/signup/", views.UserSignUp.as_view(), name="signup"),
-    path("auth/login/", views.UserLogin.as_view(), name="login"),
-    path("send-email-otp/", views.SendEmailOTP.as_view(), name="send-email-otp"),
-    path("verify-email/", views.VerifyEmail.as_view(), name="verify-email"),
-    path("reset-password/", views.ResetPassword.as_view(), name="reset-password"),
-    path("change-password/", views.ChangePassword.as_view(), name="change-password"),
+    path("auth/signup", views.UserSignUp.as_view(), name="signup"),
+    path("auth/login", views.UserLogin.as_view(), name="login"),
+    path("send-email-otp", views.SendEmailOTP.as_view(), name="send-email-otp"),
+    path("verify-email", views.VerifyEmail.as_view(), name="verify-email"),
+    path("reset-password", views.ResetPassword.as_view(), name="reset-password"),
+    path("change-password", views.ChangePassword.as_view(), name="change-password"),
 ]
