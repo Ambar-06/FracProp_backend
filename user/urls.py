a2683aab5113cbe7485f1f5411583e21f3b674c7
Ambar Rizvi <brannstrom9911@gmail.com>
2025-01-23 14:03:27 +0530

    UserProfileView

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("auth/signup", views.UserSignUp.as_view(), name="signup"),
     path("auth/login", views.UserLogin.as_view(), name="login"),
+    path("profile", views.UserProfileView.as_view(), name="profile"),
     path("send-email-otp", views.SendEmailOTP.as_view(), name="send-email-otp"),
     path("verify-email", views.VerifyEmail.as_view(), name="verify-email"),
     path("reset-password", views.ResetPassword.as_view(), name="reset-password"),
--- user/views/__init__.py
+++ user/views/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserSignUp, UserLogin
 from .auth_views import *
+from .user_profile_views import *
