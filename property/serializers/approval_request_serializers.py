413daace7b3f61fd893b3bf748fd16c47969b2ce
Ambar Rizvi <brannstrom9911@gmail.com>
2025-02-11 22:36:40 +0530

    added information related to requested by and property

--- property/serializers/approval_request_serializers.py
+++ property/serializers/approval_request_serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 
 from property.models.property_approval_request import PropertyApprovalRequest
+from property.serializers.property_serializers import PropertySerializer
 
 class ApprovalRequestViewSerializer(serializers.ModelSerializer):
+    property = serializers.SerializerMethodField("get_property")
+    requested_by = serializers.SerializerMethodField("get_requested_by")
+    
     class Meta:
         model = PropertyApprovalRequest
         exclude = ("created_at", "meta", "id")
+
+    def get_property(self, obj):
+        return PropertySerializer(obj.property).data
+    
+    def get_requested_by(self, obj):
+        return {
+            "uuid": obj.requested_by.uuid,
+            "name": obj.requested_by.first_name + " " + obj.requested_by.last_name,
+            "email": obj.requested_by.email,
+            "phone_number": obj.requested_by.phone_number,
+        }
